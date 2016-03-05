1144041b2778d518e8dd35d1962d3b7537032dc8
Stewart Yu <stewartyu@gmail.com>
2016-03-05 12:18:01 -0800

    add to cart button

--- app/js/components/products.js
+++ app/js/components/products.js
@@ -2,14 +2,29 @@ var $ = require('jquery');
 var _ = require('lodash');
 var productTemplate = require('../../templates/partials/_product.tpl');
 
+var allProducts = [];
+
+var showProducts = function(products) {
+    var $products = $('.js-products');
+    _.map(products, function(product) {
+        var compiledTemplate = _.template(productTemplate);
+        $products.append(compiledTemplate(product));
+    });
+};
+
+var bindEvents = function() {
+    $('.js-product__cta').on('click', function() {
+        var id = $(this).attr('data-id');
+    });
+};
+
 module.exports = {
     init: function() {
         $.ajax('/products').done(function(products) {
-            var $products = $('.js-products');
-            _.map(products, function(product) {
-                var compiledTemplate = _.template(productTemplate);
-                $products.append(compiledTemplate(product));
-            });
+            showProducts(products);
+            bindEvents();
+
+            allProducts = products;
         });
     }
 };
--- app/templates/partials/_product.tpl
+++ app/templates/partials/_product.tpl
@@ -2,4 +2,5 @@
     <h2 class="product__name"><%= name %></h2>
     <p class="product__description"><%= description %></p>
     <span class="product__price"><%= price %></span>
+    <button class="product__cta js-product__cta" data-id="<%= id %>">Add to Cart</button>
 </div>
