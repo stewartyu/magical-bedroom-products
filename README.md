fe33187256f7e8dfb8e0cd504368ed47e8ccfb50
Stewart Yu <stewartyu@gmail.com>
2016-03-04 22:51:37 -0800

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# README
+
+## Getting Started
+
+### Install Gulp
+
+    npm install -g gulp
+
+### Install Node Modules
+
+    npm install
+
+## Running the Project
+
+    gulp
+
+Now go to [http://localhost:3000](http://localhost:3000)!
---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+var gulp = require('gulp');
+var jsonServer = require('gulp-json-srv');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var cleanCSS = require('gulp-clean-css');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var uglify = require('gulp-uglify');
+var buffer = require('vinyl-buffer');
+var jade = require('gulp-jade');
+
+var server = jsonServer.start({
+    data: 'data/db.json',
+    deferredStart: true
+});
+
+gulp.task('sass', function() {
+    return gulp.src('./app/scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer('last 2 versions', 'ie 9'))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('scripts', function() {
+    return browserify('./app/js/app.js')
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(buffer())
+        .pipe(uglify())
+		.pipe(gulp.dest('./public/js'));
+});
+
+gulp.task('templates', function() {
+  gulp.src('./app/templates/**/*.jade')
+    .pipe(jade())
+    .pipe(gulp.dest('./public/'))
+});
+
+gulp.task('watch', function() {
+    gulp.watch('./app/scss/**/*.scss', ['sass']);
+    gulp.watch('./app/js/**/*.js', ['scripts'])
+    gulp.watch('./app/templates/**/*.jade', ['templates'])
+    gulp.watch(['db.json'], function(){
+        server.reload();
+    });
+});
+
+gulp.task('develop', function() {
+    server.start();
+});
+
+gulp.task('default', [
+    'sass',
+    'scripts',
+    'templates',
+    'develop',
+    'watch'
+]);
